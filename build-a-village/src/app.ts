49a2eb3d53e8b70bf97ed467f2aff5189e4d9d9c
tongxin.wen <tongxinwen@fintopia.tech>
2024-09-30 09:11:17 +0800

    尝试import

--- build-a-village/src/app.ts
+++ build-a-village/src/app.ts
@@ -26,12 +26,15 @@ class App {
 
         camera.attachControl(canvas, true);
         const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
-        light.intensity = 2.7;
-        BABYLON.SceneLoader.ImportMeshAsync("", "/assets/meshes/", "village.glb");
+        light.intensity = 1.0;
 
         //Comment: 通过这个函数创建了村庄后，可以导出为glb，然后在import的时候，可以直接导入。如上所示
+        // 会显得有点黑，已经在社区提问了 https://forum.babylonjs.com/t/why-do-the-meshes-looks-darker-than-in-the-original-scene-after-imported/53840
         // this.buildVillage();
+        BABYLON.SceneLoader.ImportMeshAsync("", "/assets/meshes/", "village.glb");
 
+        // put Dude in scene
+        // this.buildDude(scene);
         // hide/show the Inspector
         window.addEventListener("keydown", (ev) => {
             // Shift+Alt+I
@@ -78,9 +81,59 @@ class App {
     }
 
     /******Build Functions***********/
+    buildDude = (scene: Scene) => {
+        const walk = function (turn, dist) {
+            this.turn = turn;
+            this.dist = dist;
+        }
+
+        const track = [];
+        track.push(new walk(86, 7));
+        track.push(new walk(-85, 14.8));
+        track.push(new walk(-93, 16.5));
+        track.push(new walk(48, 25.5));
+        track.push(new walk(-112, 30.5));
+        track.push(new walk(-72, 33.2));
+        track.push(new walk(42, 37.5));
+        track.push(new walk(-98, 45.2));
+        track.push(new walk(0, 47))
+
+        // Dude
+        BABYLON.SceneLoader.ImportMeshAsync("him", "/assets/meshes/Dude/", "Dude.babylon", scene).then((result) => {
+            var dude = result.meshes[0];
+            dude.scaling = new BABYLON.Vector3(0.008, 0.008, 0.008);
+
+            dude.position = new BABYLON.Vector3(-6, 0, 0);
+            dude.rotate(BABYLON.Axis.Y, BABYLON.Tools.ToRadians(-95), BABYLON.Space.LOCAL);
+            const startRotation = dude.rotationQuaternion.clone();
+
+            scene.beginAnimation(result.skeletons[0], 0, 100, true, 1.0);
+
+            let distance = 0;
+            let step = 0.015;
+            let p = 0;
+
+            scene.onBeforeRenderObservable.add(() => {
+                dude.movePOV(0, 0, step);
+                distance += step;
+
+                if (distance > track[p].dist) {
+
+                    dude.rotate(BABYLON.Axis.Y, BABYLON.Tools.ToRadians(track[p].turn), BABYLON.Space.LOCAL);
+                    p += 1;
+                    p %= track.length;
+                    if (p === 0) {
+                        distance = 0;
+                        dude.position = new BABYLON.Vector3(-6, 0, 0);
+                        dude.rotationQuaternion = startRotation.clone();
+                    }
+                }
+
+            })
+        });
+    }
     buildVillage = () => {
         const ground = this.buildGround();
-
         const detached_house = this.buildHouse(1);
         detached_house.rotation.y = -Math.PI / 16;
         detached_house.position.x = -6.8;
@@ -93,31 +146,31 @@ class App {
 
         const places = []; //each entry is an array [house type, rotation, x, z]
         places.push([1, -Math.PI / 16, -6.8, 2.5]);
+        places.push([1, 15 * Math.PI / 16, -4.1, -1]);
+        places.push([1, 5 * Math.PI / 4, 0, -1]);
+        places.push([1, Math.PI + Math.PI / 2.5, 0.5, -3]);
+        places.push([1, Math.PI + Math.PI / 2.25, 0.75, -7]);
+        places.push([1, Math.PI / 1.95, 4.5, -3]);
+        places.push([1, Math.PI / 1.9, 4.75, -7]);
+        places.push([1, -Math.PI / 3, 6, 4]);
         places.push([2, -Math.PI / 16, -4.5, 3]);
         places.push([2, -Math.PI / 16, -1.5, 4]);
         places.push([2, -Math.PI / 3, 1.5, 6]);
         places.push([2, 15 * Math.PI / 16, -6.4, -1.5]);
-        places.push([1, 15 * Math.PI / 16, -4.1, -1]);
         places.push([2, 15 * Math.PI / 16, -2.1, -0.5]);
-        places.push([1, 5 * Math.PI / 4, 0, -1]);
-        places.push([1, Math.PI + Math.PI / 2.5, 0.5, -3]);
         places.push([2, Math.PI + Math.PI / 2.1, 0.75, -5]);
-        places.push([1, Math.PI + Math.PI / 2.25, 0.75, -7]);
         places.push([2, Math.PI / 1.9, 4.75, -1]);
-        places.push([1, Math.PI / 1.95, 4.5, -3]);
         places.push([2, Math.PI / 1.9, 4.75, -5]);
-        places.push([1, Math.PI / 1.9, 4.75, -7]);
         places.push([2, -Math.PI / 3, 5.25, 2]);
-        places.push([1, -Math.PI / 3, 6, 4]);
 
         //Create instances from the first two that were built
         const houses = [];
         for (let i = 0; i < places.length; i++) {
             if (places[i][0] === 1) {
-                houses[i] = detached_house.createInstance("house" + i);
+                houses[i] = detached_house.createInstance("detached" + i);
             }
             else {
-                houses[i] = semi_house.createInstance("house" + i);
+                houses[i] = semi_house.createInstance("semi" + i);
             }
             houses[i].rotation.y = places[i][1];
             houses[i].position.x = places[i][2];
